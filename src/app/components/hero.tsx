3208cb976511a324f066ccdb0f35857649debe25
Royce Williams <roycewilliamsj@gmail.com>
2025-05-21 13:49:06 -0700

    small text added

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -6,12 +6,12 @@ export default function Hero () {
     return (
         <section className="tracking-widest uppercase">
             <div className="mt-52  text-white">
-                <h1 className="text-3xl xl:text-4xl">Supercharge your inbox</h1>
-                <p className="mt-8 xl:mt-12 text-xs md:text-sm ">Organize, secure, and streamline your legal inbox effortlessly.</p>
+                <h1 className="text-3xl xl:text-6xl">Supercharge your inbox</h1>
+                <p className="mt-8 xl:mt-12 text-sm ">Organize, secure, and streamline your legal inbox effortlessly.</p>
             </div>
             <div className="flex mt-8 text-white gap-x-8 text-xs">
-                <button className="uppercase px-8 py-4 bg-black rounded-full shadow-lg">Join Waitlist</button>
-                <button className="text-black uppercase px-8 py-4 bg-white rounded-full shadow-lg">Contact Us</button>
+                <button className="uppercase px-6 py-3 bg-black rounded-full shadow-lg">Join Waitlist</button>
+                <button className="text-black uppercase px-6 py-3 bg-white rounded-full shadow-lg">Contact Us</button>
             </div>
         </section>
     )
